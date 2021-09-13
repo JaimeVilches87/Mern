17d848f3dc1b7d07cf9576fbadfaa72b64d756f4
JaimeVilches87 <jaimevilches87@gmail.com>
2021-09-13 18:23:15 -0400

    added typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,27 @@
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+type Query {
+
+
+}
+type Mutation {
+
+
+}
+
+type User {
+
+}
+
+type Book {
+
+
+}
+
+type Auth {
+
+
+}`;
+
+module.exports = typeDefs;
